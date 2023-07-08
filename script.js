b4eec0c993cf14e264e6675e12ff97ca4bc7c25a
Gigalophoantropo <ugartegarecajuansteven25@gmail.com>
2023-07-08 19:04:15 +0200

    Monday Left Me Broken

---
+++ script.js
@@ -0,0 +1,24 @@
+function MondayCatJumpscare(){
+    document.getElementById("69").setAttribute("src", "Images/MondayCatGif.gif")
+    document.getElementById("69").setAttribute("height", "500")
+}
+
+function MondayLeftMeBroken(){
+    let MondayAudio = document.createElement("audio")
+    MondayAudio.innerHTML = `<audio autoplay loop><source src="MondayLeftMeBrokenCat.mp3#t=00:00:42"></audio>`
+    document.body.appendChild(MondayAudio)
+}
+
+var Monday = "Monday Left Me Broken"
+let Tuesday = "\nTuesday I was Through With Hoping"
+const Wednesday = "\nWednesday My Empty Arms Were Open"
+
+var SUS
+let sus
+
+
+SUS = 69
+sus = NaN
+
+
+console.log(Monday, Tuesday, Wednesday, SUS, sus)
